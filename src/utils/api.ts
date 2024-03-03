86d185fa22566c14d3be43be29f52e060c2d0de0
jurmaev <smartemii03@gmail.com>
2024-03-03 20:29:29 +0500

    refactor

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,11 @@
-import axios, { AxiosError, AxiosRequestConfig } from 'axios';
+import axios, { AxiosError } from 'axios';
 import md5 from 'md5';
 
 const baseURL = 'http://api.valantis.store:40000/';
 
 export const api = axios.create({ baseURL });
 
-api.interceptors.request.use((config: AxiosRequestConfig) => {
+api.interceptors.request.use((config) => {
   if (config.headers) {
     const date = new Date();
     config.headers['X-Auth'] = md5(
